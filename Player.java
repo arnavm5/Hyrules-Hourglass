45ffcf090547e85f1b2bee041d6610abb6dcdcf4
Shadowtrap <arnav.mittal09@gmail.com>
2019-05-30 15:45:51 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,280 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.util.Calendar;
+import javax.swing.ImageIcon;
+
+
+public class Player extends Character{
+
+    private boolean isInside;
+    private boolean left, right, up, down, shift;
+    private int lastKey;
+    private double timePressed;
+
+    public Player(int x, int y, int health, int speed, boolean isInside) {
+        super(x, y, health, speed, false);
+        this.isInside = isInside;
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+        shift = false;
+        lastKey = 1;
+        timePressed = 0;
+    }
+
+    public boolean isInside() {
+        return isInside;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public boolean isShift() {
+        return shift;
+    }
+
+    public void setInside(boolean inside) {
+        isInside = inside;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void keyPressedPlayer(int key){
+        if (key == 87) {
+            up = true;
+            down = false;
+            left = false;
+            right = false;
+        }
+        else if (key == 83) {
+            down = true;
+            up = false;
+            left = false;
+            right = false;
+        }
+        else if (key == 65) {
+            left = true;
+            up = false;
+            down = false;
+            right = false;
+        }
+        else if (key == 68) {
+            right = true;
+            left = false;
+            up = false;
+            down = false;
+        }
+        if (key == 16){
+            shift = true;
+        }
+    }
+
+    public void keyReleasedPlayer(int key){
+        //System.out.println(timePressed);
+        if (key == 87) {
+            up = false;
+            lastKey = 1;
+        }
+        if (key == 83) {
+            down = false;
+            lastKey = 2;
+        }
+        if (key == 65) {
+            left = false;
+            lastKey = 3;
+        }
+        if (key == 68) {
+            right = false;
+            lastKey = 4;
+        }
+        if(key == 16){
+            shift = false;
+        }
+    }
+
+    public void timePressedMove(){
+        if(up || down || left || right){
+            timePressed += 0.4;
+            if(timePressed > 11) {
+                timePressed = timePressed % 11;
+            }
+        }
+        else{
+            timePressed = 0;
+        }
+    }
+
+    public void movePlayer() {
+        if (isInside) {
+            if (up) {
+                setY(getY() - getSpeed());
+            }
+            if (down) {
+                setY(getY() + getSpeed());
+            }
+            if (left) {
+                setX(getX() - getSpeed());
+            }
+            if (right) {
+                setX(getX() + getSpeed());
+            }
+        }
+    }
+
+    public void moveScene(ArrayList<Rectangle> gameSceneObjects, Rectangle r) {
+        if(!isInside){
+            if(gameSceneObjects.size() > 0) {
+                if (up) {
+                    for (Rectangle o : gameSceneObjects) {
+                        o.setY(o.getY() + getSpeed());
+                    }
+                    r.setY(r.getY() + getSpeed());
+                }
+                if (down) {
+                    for (Rectangle o : gameSceneObjects) {
+                        o.setY(o.getY() - getSpeed());
+                    }
+                    r.setY(r.getY() - getSpeed());
+                }
+                if (left) {
+                    for (Rectangle o : gameSceneObjects) {
+                        o.setX(o.getX() + getSpeed());
+                    }
+                    r.setX(r.getX() + getSpeed());
+                }
+                if (right) {
+                    for (Rectangle o : gameSceneObjects) {
+                        o.setX(o.getX() - getSpeed());
+                    }
+                    r.setX(r.getX() - getSpeed());
+                }
+            }
+        }
+    }
+
+    public void paintPlayer(Graphics g){
+        Graphics2D g2d = (Graphics2D)g;
+        ImageIcon link = new ImageIcon(Player.class.getResource("Assets/Link/tile000.png"));
+
+        if(up) {
+            for(int i = 48; i <= timePressed + 48; i++) {
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile0" + i + ".png"));
+            }
+        }
+        else if(down) {
+            for(int i = 24; i <= timePressed + 24; i++) {
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile0" + i + ".png"));
+            }
+        }
+        else if(left) {
+            for(int i = 36; i <= timePressed + 36; i++) {
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile0" + i + ".png"));
+            }
+        }
+        else if(right) {
+            for(int i = 12; i <= timePressed + 12; i++) {
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile0" + i + ".png"));
+            }
+        }
+        else{
+            if(lastKey == 1){
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile000.png"));
+            }
+            else if(lastKey == 2){
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile024.png"));
+            }
+            else if(lastKey == 3){
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile037.png"));
+            }
+            else if(lastKey == 4){
+                link = new ImageIcon(Player.class.getResource("Assets/Link/tile013.png"));
+            }
+        }
+        g2d.drawImage(link.getImage(), getX(), getY(), getW(), getH(), null);
+        g.setColor(Color.BLUE);
+        //g.fillRect(getX(), getY(), getW(), getH());
+    }
+
+    public void sceneCollision(Rectangle room, int wallPixLenX, int wallPixLenY){
+        if(getX() < room.getX() + wallPixLenX){
+            setX(room.getX() + wallPixLenX);
+        }
+        if(getX() + getW() > room.getX() + room.getW() - wallPixLenX){
+            setX(room.getX() + room.getW() - getW() - wallPixLenX);
+        }
+        if(getY() < room.getY() + wallPixLenY){
+            setY(room.getY() + wallPixLenY);
+        }
+        if(getY() + getH() > room.getY() + room.getH() - wallPixLenY){
+            setY(room.getY() + room.getH() - getH() - wallPixLenY);
+        }
+    }
+
+    public void movementSwitch(Rectangle scene){
+        if(scene.isOnEdge()){
+            isInside = true;
+        }
+        else{
+            isInside = false;
+        }
+    }
+
+     public void recCollision(Rectangle r, int wallPixLenX, int wallPixLenY){
+        int linkRX = getX() + wallPixLenX;
+        int linkRW = getX() + getW() - wallPixLenX;
+        int linkRY = getY() + wallPixLenY;
+        int linkRH = getY() + getH();
+
+        if(r.getX() < linkRW && r.getX() + r.getW() > linkRX && r.getY() < linkRH && r.getY() + r.getH() > linkRY) {
+                if (linkRX - getSpeed() <= r.getX() + r.getW() && linkRX + getSpeed() > r.getX() + r.getW() * 0.90 && left) {
+                    setX(r.getX() + r.getW() - wallPixLenX);
+                    System.out.println("X Right");
+                }
+                else if (linkRW + getSpeed() >= r.getX() && linkRW - getSpeed() < r.getX() + r.getW() * 0.10 && right) {
+                    setX(r.getX() - getW() + wallPixLenX);
+                    System.out.println("X Left");
+                }
+                else if (linkRY - getSpeed() <= r.getY() + r.getH() && linkRY + getSpeed() > r.getY() + r.getH() * 0.90 && up) {
+                    setY(r.getY() + r.getH() - wallPixLenY);
+                    System.out.println("Y Down");
+                }
+                else if (linkRH + getSpeed() >= r.getY() && linkRH - getSpeed() < r.getY() + r.getH() * 0.10 && down) {
+                    setY(r.getY() - getH());
+                    System.out.println("Y Up");
+                }
+        }
+    }
+
+
+}
---
+++ Scene.java
@@ -0,0 +1,392 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Scene {
+
+    private ArrayList<Rectangle> houseObjects, villageObjects, castleObjects, forestObjects;;
+    private boolean inHouse, inVillage, inCastle,inForest, start, story1, enterCastle, villainTalks, question1 ,villainDec, end, win, lose, controls, space,spaceIncrement, enter;
+    private Rectangle houseExit , villageExitCastle, villageExitForest;
+    private Rectangle houseEnter, villageEnterCastle, villageEnterForest;
+    private Rectangle textBox;
+    private Font text;
+    private double timeSceneChange;
+    private Character zelda;
+    private int countSpaces;
+    private boolean isZeldaMoving;
+
+    public Scene(){
+        houseObjects = new ArrayList<Rectangle>();
+        villageObjects = new ArrayList<Rectangle>();
+        castleObjects = new ArrayList<Rectangle>();
+        forestObjects = new ArrayList<Rectangle>();
+        inHouse = true;
+        inVillage = false;
+        inCastle = false;
+        inForest = false;
+        start = true;
+        story1 = true;
+        enterCastle = true;
+        question1 = false;
+        villainTalks = false;
+        villainDec = false;
+        end = false;
+        win = false;
+        lose = false;
+        controls = true;
+        space = false;
+        enter = false;
+        countSpaces = 0;
+        timeSceneChange = 0;
+        houseExit  = new Rectangle(0,0,0,0, false);
+        villageExitCastle = new Rectangle(0,0,0,0, false);
+        villageExitForest = new Rectangle(0,0,0,0, false);
+        houseEnter = new Rectangle(0,0,0,0, false);
+        villageEnterCastle = new Rectangle(0,0,0,0, false);
+        villageEnterForest = new Rectangle(0,0,0,0, false);
+        textBox = new Rectangle(0,0,0,0, false);
+        text = new Font("Ariel", Font.PLAIN, 20);
+        zelda = new Character(0,0,0,0, false);
+        spaceIncrement=false;
+        isZeldaMoving = false;
+
+    }
+
+    public ArrayList<Rectangle> getHouseSceneObjects(Rectangle background){
+       Rectangle firePlace1, firePlace2, workshopTable, table, fence1, fence2, picket;
+       firePlace1 = new Rectangle(182 + background.getX(), 128 + background.getY(), 25, 30, false);
+       firePlace2 = new Rectangle(250 + background.getX(), 128 + background.getY(), 29, 30, false);
+       workshopTable = new Rectangle(75 + background.getX(), 70 + background.getY(), 44, 36, false);
+       table = new Rectangle(scale(160, 1.875) + background.getX() - 5, scale(80,2) + background.getY(), scale(207, 1.875) - scale(160, 1.875), scale(110,2) - scale(80,2), false);
+       fence1 = new Rectangle(scale(32, 1.875) + background.getX(), scale(80,2) + background.getY(), scale(86, 1.875) - scale(32, 1.875), scale(87,2) - scale(80,2), false);
+       picket = new Rectangle(scale(80, 1.875) + background.getX(), scale(88,2) + background.getY(), scale(86, 1.875) - scale(80, 1.875), scale(95,2) - scale(88,2), false);
+       fence2 = new Rectangle(scale(80, 1.875) + background.getX(), scale(128,2) + background.getY(), scale(86, 1.875) - scale(80, 1.875), scale(143,2) - scale(128,2), false);
+       textBox = new Rectangle(background.getX() - 10, background.getY() + background.getH() - 50, background.getW() + 20, 100, false);
+       houseExit = new Rectangle(scale(111, 1.875) + background.getX(), scale(148,2) + background.getY(), scale(130, 1.875) - scale(111, 1.875), scale(159,2) - scale(155,2), false);
+       houseObjects.add(firePlace1);
+       houseObjects.add(firePlace2);
+       houseObjects.add(workshopTable);
+       houseObjects.add(table);
+       houseObjects.add(fence1);
+       houseObjects.add(picket);
+       houseObjects.add(fence2);
+       houseObjects.add(textBox);
+       houseObjects.add(houseExit);
+       return houseObjects;
+    }
+
+    public ArrayList<Rectangle> getVillageSceneObjects(Rectangle background) {
+        Rectangle firePlace1;
+        houseEnter = new Rectangle(scale(425, 1.5) + background.getX(), scale(385, 1.45) + background.getY(), scale(430, 1.5) - scale(425, 1.5), scale(390, 1.45) - scale(385, 1.45), false);
+        villageExitCastle = new Rectangle(scale(255, 1.5) + background.getX(), scale(70, 1.45) + background.getY(), scale(315, 1.5) - scale(255, 1.5), scale(75, 1.45) - scale(70, 1.45), false);
+        villageExitForest = new Rectangle(scale(55, 1.5) + background.getX(), scale(605, 1.45) + background.getY(), scale(60, 1.5) - scale(55, 1.5), scale(635, 1.45) - scale(605, 1.45), false);
+        if(story1) {
+            zelda = new Character(375 + background.getX(), 650 + background.getY(), 100, 2, false);
+        }
+        villageObjects.add(houseEnter);
+        villageObjects.add(villageExitCastle);
+        villageObjects.add(villageExitForest);
+        villageObjects.add(zelda);
+        return villageObjects;
+    }
+
+    public ArrayList<Rectangle> getCastleSceneObjects(Rectangle background) {
+        Rectangle firePlace1;
+        villageEnterCastle = new Rectangle(scale(485, 1.3) + background.getX(), scale(505, 1.55) + background.getY(), scale(525, 1.3) - scale(485, 1.3), scale(510, 1.55) - scale(505, 1.55), false);
+        if(!story1 && enterCastle) {
+            zelda = new Character(375 + background.getX(), 650 + background.getY(), 100, 2, false);
+        }
+        castleObjects.add(villageEnterCastle);
+        castleObjects.add(zelda);
+        return castleObjects;
+    }
+
+    public ArrayList<Rectangle> getForestSceneObjects(Rectangle background) {
+        Rectangle firePlace1;
+        villageEnterForest = new Rectangle(scale(125, 1.05) + background.getX(), scale(15, 1.6) + background.getY(), scale(165, 1.05) - scale(125, 1.05), scale(20, 1.6) - scale(15, 1.6), false);
+        forestObjects.add(villageExitForest);
+        return forestObjects;
+    }
+
+    private static int scale(int x, double scale){
+        return (int)(x * scale);
+    }
+
+    public boolean isInHouse() {
+        return inHouse;
+    }
+
+    public void setInHouse(boolean inHouse) {
+        this.inHouse = inHouse;
+    }
+
+    public boolean isInVillage() {
+        return inVillage;
+    }
+
+    public void setInVillage(boolean inVillage) {
+        this.inVillage = inVillage;
+    }
+
+    public boolean isInCastle() {
+        return inCastle;
+    }
+
+    public void setInCastle(boolean inCastle) {
+        this.inCastle = inCastle;
+    }
+
+    public boolean isInForest() {
+        return inForest;
+    }
+
+    public void setInForest(boolean inForest) {
+        this.inForest = inForest;
+    }
+
+    public void sceneChangeExit(Player link){
+        int linkRX = link.getX() + link.getSpeed() + 4;
+        int linkRW = link.getX() + link.getW() - link.getSpeed() - 4;
+        int linkRY = link.getY() - link.getSpeed() - 4;
+        int linkRH = link.getY() + link.getH() + link.getSpeed() + 4;
+        if(inHouse && houseExit.getX() < linkRW && houseExit.getX() + houseExit.getW() > linkRX && houseExit.getY() < linkRH && houseExit.getY() + houseExit.getH() > linkRY && link.isDown()){
+            System.out.println("Exit");
+            inHouse = false;
+            inVillage = true;
+            link.setInside(false);
+            link.setX(615);
+            link.setY(230);
+        }
+        else if(inVillage && villageExitCastle.getX() < linkRW && villageExitCastle.getX() + villageExitCastle.getW() > linkRX && villageExitCastle.getY() < linkRH && villageExitCastle.getY() + villageExitCastle.getH() > linkRY && link.isUp()){
+            System.out.println("Exit");
+            inVillage = false;
+            inCastle = true;
+            link.setInside(false);
+            link.setX(515);
+            link.setY(300);
+        }
+        else if(inVillage && villageExitForest.getX() < linkRW && villageExitForest.getX() + villageExitForest.getW() > linkRX && villageExitForest.getY() < linkRH && villageExitForest.getY() + villageExitForest.getH() > linkRY && link.isLeft()){
+            System.out.println("Exit");
+            inVillage = false;
+            inForest = true;
+            link.setInside(true);
+            link.setX(150);
+            link.setY(20);
+        }
+    }
+
+    public void sceneChangeEnter(Player link){
+        int linkRX = link.getX() + link.getSpeed() + 4;
+        int linkRW = link.getX() + link.getW() - link.getSpeed() - 4;
+        int linkRY = link.getY() - link.getSpeed() - 4;
+        int linkRH = link.getY() + link.getH() + link.getSpeed() + 4;
+        if(inVillage && houseEnter.getX() < linkRW && houseEnter.getX() + houseEnter.getW() > linkRX && houseEnter.getY() < linkRH && houseEnter.getY() + houseEnter.getH() > linkRY && link.isUp()){
+            System.out.println("Enter");
+            inHouse = true;
+            inVillage = false;
+            link.setInside(true);
+            link.setX(475);
+            link.setY(300);
+        }
+        else if(inCastle && villageEnterCastle.getX() < linkRW && villageEnterCastle.getX() + villageEnterCastle.getW() > linkRX && villageEnterCastle.getY() < linkRH && villageEnterCastle.getY() + villageEnterCastle.getH() > linkRY && link.isDown()){
+            System.out.println("Enter");
+            inVillage = true;
+            inCastle = false;
+            link.setInside(false);
+            link.setX(400);
+            link.setY(95);
+        }
+        else if(inForest && villageEnterForest.getX() < linkRW && villageEnterForest.getX() + villageEnterForest.getW() > linkRX && villageEnterForest.getY() < linkRH && villageEnterForest.getY() + villageEnterForest.getH() > linkRY && link.isUp()){
+            System.out.println("Enter");
+            inVillage = true;
+            inForest = false;
+            link.setInside(false);
+            link.setX(575);
+            link.setY(125);
+        }
+
+    }
+
+    public void paintScene(Graphics g, Rectangle r, int scene){
+        Graphics2D g2d = (Graphics2D)g;
+        ImageIcon back;
+        if(isInHouse() && scene == 0) {
+            back = new ImageIcon(Scene.class.getResource("Assets/Scenes/house.png"));
+            g2d.drawImage(back.getImage(), r.getX(), r.getY(), r.getW(), r.getH(), null);
+        }
+        else if(isInVillage() && scene == 1) {
+            g.setColor(Color.BLACK);
+            back = new ImageIcon(Scene.class.getResource("Assets/Scenes/village.png"));
+            g2d.drawImage(back.getImage(), r.getX(), r.getY(), r.getW(), r.getH(), null);
+        }
+        else if(isInCastle() && scene == 2) {
+            back = new ImageIcon(Scene.class.getResource("Assets/Scenes/castle.jpg"));
+            g2d.drawImage(back.getImage(), r.getX(), r.getY(), r.getW(), r.getH(), null);
+        }
+        else if(isInForest() && scene == 3) {
+            back = new ImageIcon(Scene.class.getResource("Assets/Scenes/forest.png"));
+            g2d.drawImage(back.getImage(), r.getX(), r.getY(), r.getW(), r.getH(), null);
+        }
+        if(isInVillage() && zelda.getY() >= r.getY() && enterCastle) {
+            Character.paintZelda(g, 2, zelda);
+        }
+        else{
+
+        }
+    }
+
+    public void paintText(Graphics g){
+        Graphics2D g2d = (Graphics2D)g;
+        ImageIcon box = new ImageIcon(Scene.class.getResource("Assets/Extras/TextBox.jpg"));
+        g.setFont(text);
+        if(start && controls && inHouse){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.WHITE);
+            g.drawString("Controls: W-A-S-D to move, Shift to run.", textBox.getX() + 15, textBox.getY() + 30);
+        }
+        else if(start && inHouse){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.WHITE);
+            g.drawString("???: Link, we will be late to the festival.", textBox.getX() + 15, textBox.getY() + 30);
+        }
+        else if(story1 && inVillage){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.WHITE);
+            g.drawString("Zelda: I will see you at the Hourglass Festival in ", textBox.getX() + 15, textBox.getY() + 30);
+            g.drawString(".", textBox.getX() + 133, textBox.getY() + 50);
+            g.setColor(Color.CYAN);
+            g.drawString("Hyrule Castle", textBox.getX() + 15, textBox.getY() + 50);
+        }
+        else if(inCastle && enterCastle){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.WHITE);
+            g.drawString("Link: Hey, where did everyone go?", textBox.getX() + 15, textBox.getY() + 30);
+        }
+        else if(inCastle && villainTalks){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.MAGENTA);
+            g.drawString("???: MUWHAHAHAHA, I have Zelda and the", textBox.getX() + 15, textBox.getY() + 30);
+            g.drawString("Hourglass. No one can stop me from taking ", textBox.getX() + 15, textBox.getY() + 50);
+            g.drawString("over Hyrule now.", textBox.getX() + 15, textBox.getY() + 73);
+        }
+        else if(inCastle && question1){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.WHITE);
+            g.drawString("Link: WHO ARE YOU AND WHERE IS ZELDA??", textBox.getX() + 15, textBox.getY() + 30);
+        }
+        else if(inCastle && villainDec){
+            g2d.drawImage(box.getImage(), textBox.getX(), textBox.getY(), textBox.getW(), textBox.getH(), null);
+            g.setColor(Color.MAGENTA);
+            g.drawString("???: I AM VAATI, AND I WILL TAKE OVER ", textBox.getX() + 15, textBox.getY() + 30);
+            g.drawString("HYRULE. MWAHAHAHA", textBox.getX() + 15, textBox.getY() + 50);
+        }
+    }
+
+    public void sceneKeyPressed(int key){
+        if(key == 32){
+            space = true;
+            
+        }
+        if (key == 10) {
+            enter = true;
+        }
+    }
+
+    public void sceneKeyReleased(int key){
+        if(key == 32){
+            space = false;
+            if(spaceIncrement)
+                countSpaces++;
+        }
+        if (key == 10) {
+            enter = false;
+        }
+    }
+
+    public void storyHandler(Player link){
+        if(countSpaces == 0){
+            start = true;
+            controls = true;
+        }
+        else if(countSpaces == 1){
+            controls = false;
+        }
+        else if(countSpaces == 2){
+            start = false;
+        }
+        else if(countSpaces == 3){
+            story1 = false;
+            isZeldaMoving = true;
+            if(zelda.getY() + zelda.getH() < 0){
+                isZeldaMoving = false;
+            }
+        }
+        else if(countSpaces == 4){
+            enterCastle = false;
+            if(link.getY() < 200){
+                villainTalks = true;
+            }
+        }
+        else if(countSpaces == 5){
+            villainTalks = false;
+            question1 = true;
+        }
+        else if(countSpaces == 6){
+            question1 = false;
+            villainDec = true;
+        }
+        else if(countSpaces == 7){
+            villainDec = false;
+        }
+
+        System.out.println(countSpaces);
+        boolean notWalk1 = countSpaces == 2 && story1 && inVillage;
+        boolean notWalk2 = countSpaces == 3 && enterCastle && inCastle;
+        boolean notWalk3 = countSpaces == 4 && villainTalks && inCastle;
+        boolean notWalk4 = countSpaces == 5 && question1 && inCastle;
+        boolean notWalk5 = countSpaces == 6 && villainDec && inCastle;
+        if(isZeldaMoving){
+            link.setSpeed(0);
+            link.setUp(false);
+            link.setDown(false);
+            link.setLeft(false);
+            link.setRight(false);
+        }
+        else if(countSpaces <= 1 || notWalk1 || notWalk2 || notWalk3 || notWalk4 || notWalk5){
+            link.setSpeed(0);
+            link.setUp(false);
+            link.setDown(false);
+            link.setLeft(false);
+            link.setRight(false);
+            spaceIncrement = true;
+        }
+        else{
+            isZeldaMoving = false;
+            spaceIncrement = false;
+            if(link.isShift())
+                link.setSpeed(4);
+            else{
+                link.setSpeed(3);
+            }
+        }
+
+    }
+
+    public void autoCharacters(Rectangle scene, Player link, int x){
+        boolean zeldaMove1 = countSpaces == 2 || countSpaces == 3 && inVillage;
+        if(zeldaMove1 && x == 1){
+            if(zelda.getY() >= scene.getY()){
+                zelda.setY(zelda.getY() - zelda.getSpeed());
+                if(zelda.getY() <= scene.getY()){
+                    villageObjects.remove(zelda);
+                    castleObjects.add(zelda);
+                }
+            }
+        }
+       // if(villainTalks && !question1){
+        //    if(scene.getY() < -100){
+       //         link.setUp(true);
+         //   }
+        //}
+    }
+}
